22fb0147390bd1a6375d051cef8fd02e4d785e76
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 18:58:49 -0400

    subiendo el primer ejercicio

---
+++ Ejerci1/Ejerci1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" ");
+
+        int numero1;
+        int numero2;
+        int resultado;
+
+        Console.WriteLine("Ingrese el primer número:");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+
+        resultado = numero1 + numero2;
+
+        Console.WriteLine("El resultado de la suma es: " + resultado);
+
+        Console.WriteLine("La suma de " + numero1 + " y " + numero2 + " es " + resultado);
+    }
+}
