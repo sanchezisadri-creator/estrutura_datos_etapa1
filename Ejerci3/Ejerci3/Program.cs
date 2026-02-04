408fe5e86e83d25eeb4aaae8a0ef4bce84f5924e
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 19:54:26 -0400

    el 3

---
+++ Ejerci3/Ejerci3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" ");
+
+        int numero1;
+        int numero2;
+        int resultado;
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+        resultado = numero1 * numero2;
+
+        Console.WriteLine("El resultado de la multiplicacion es: " + resultado);
+
+        
+
+    }
+}
