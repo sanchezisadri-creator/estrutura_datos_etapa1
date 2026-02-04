c2dcf7703f79a9d86efef00380b70bc1e990a14d
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 19:23:52 -0400

    el segundo

---
+++ ejerci2/ejerci2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class program
+    {
+    static void Main()
+    {
+        Console.WriteLine(" ");
+
+        int numero1;
+        int numero2;
+        int resultado;
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+
+        resultado = numero1 - numero2;
+
+        Console.WriteLine("El resultado de la resta es: " + resultado);
+
+    }
+}
