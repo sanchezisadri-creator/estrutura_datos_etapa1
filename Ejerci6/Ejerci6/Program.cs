0562128661315241a6c5bf7b5f2656e4fdb9875a
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 20:26:16 -0400

    subiendo el 6to

---
+++ Ejerci6/Ejerci6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = 1;
+
+        while (numero <= 100)
+        {
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine(numero);
+            }
+
+            numero = numero + 1;
+        }
+    }
+}
