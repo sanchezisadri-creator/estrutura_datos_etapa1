1e98069e31a1043626d122c1c30f76ad50cb887f
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-05 19:17:33 -0400

    Subiento el 5to (actualizacion)

---
+++ Ejerci1_de_segunda_hoja/Ejerci1_de_segunda_hoja/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int inicio;
+        int final;
+
+        Console.WriteLine("Ingrese el numero de inicio: ");
+        inicio = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el numero final: ");
+        final = Convert.ToInt32(Console.ReadLine());
+
+        while (inicio <= final)
+        {
+            Console.WriteLine(inicio);
+            inicio = inicio + 1;
+        }
+    }
+}
