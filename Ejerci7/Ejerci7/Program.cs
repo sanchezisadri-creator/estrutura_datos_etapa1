8d5ff5edcd1f4c515ca807062c35fafa9c5406f8
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 20:33:41 -0400

    subiendo el 7

---
+++ Ejerci7/Ejerci7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" ");
+
+        int numero = 1;
+        while (numero <= 100)
+        {
+            if (numero % 2 != 0)
+            {
+                Console.WriteLine(numero);
+
+            }
+            numero = numero + 1;
+        }
+    }
+}
