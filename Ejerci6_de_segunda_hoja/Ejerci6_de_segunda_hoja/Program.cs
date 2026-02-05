e87a91e57c31c532113df45190854e7dc744fce5
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-05 19:30:24 -0400

    Subiendo 6to

---
+++ Ejerci6_de_segunda_hoja/Ejerci6_de_segunda_hoja/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        int inicio;
+        int final;
+        int suma = 0;
+
+        Console.WriteLine("Ingrese el numero de inicio: ");
+        inicio = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el numero final: ");
+        final = Convert.ToInt32(Console.ReadLine());
+
+        while (inicio <= final)
+        {
+            suma = suma + inicio;
+            inicio = inicio + 1;
+        }
+
+        Console.WriteLine("La suma de los numeros es: " + suma);
+    }
+}
