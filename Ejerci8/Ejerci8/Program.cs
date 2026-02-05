ecd9b48dee712a56838c7d6a61c841816bc940bd
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 20:39:09 -0400

    El 8vo (Hasta aqui por hoy)

---
+++ Ejerci8/Ejerci8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero es par.");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar.");
+        }
+    }
+}
