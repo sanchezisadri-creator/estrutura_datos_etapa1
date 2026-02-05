67e876045c10b328716074d809609d1770b3d12c
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-05 19:00:36 -0400

    subiendo el 2do

---
+++ Ejerci2_de_segunda_hoja/Ejerci2_de_segunda_hoja/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+
+        Console.WriteLine("Ingrese para saber si es positivo o negativo: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El numero es positivo (+).");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El numero es negativo (-).");
+        }
+        else
+        {
+            Console.WriteLine("El numero es neutro (0).");
+        }
+    }
+}
