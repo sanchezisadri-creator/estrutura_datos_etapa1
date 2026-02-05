71f85f2a50427e64369b7e187035492c559714be
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-04 20:16:14 -0400

    subiendo el 5to

---
+++ Ejerci5/Ejerci5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine(" ");
+
+        string nombre;
+        int edad;
+
+        Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("Ingrese su edad: ");
+        edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine($"Hola {nombre}, eres mayor.");
+        }
+        else
+        {
+            Console.WriteLine($"Hola {nombre}, eres menor .");
+        }
+    }
+}
