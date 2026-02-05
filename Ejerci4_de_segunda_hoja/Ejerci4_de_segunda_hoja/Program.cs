d40996a2c3599b65cd3c0c79cfa58eb23e264100
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-05 19:09:45 -0400

    Subiendo el 4to

---
+++ Ejerci4_de_segunda_hoja/Ejerci4_de_segunda_hoja/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+        int i = 1;
+
+        Console.WriteLine("Ingrese un numero que necesites saber si es perfecto: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (i < numero)
+        {
+            if (numero % i == 0)
+            {
+                suma = suma + i;
+            }
+
+            i = i + 1;
+        }
+
+        if (suma == numero)
+        {
+            Console.WriteLine("El numero es perfecto.");
+        }
+        else
+        {
+            Console.WriteLine("El numero no es perfecto.");
+        }
+    }
+}
