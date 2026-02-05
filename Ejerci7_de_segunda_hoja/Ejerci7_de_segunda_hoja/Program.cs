f8c875e3f8994965cd2eb4b5389d8e3ba80cc78f
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-05 19:34:49 -0400

    subiendo el 7mo

---
+++ Ejerci7_de_segunda_hoja/Ejerci7_de_segunda_hoja/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int factorial = 1;
+        int i = 1;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero < 0)
+        {
+            Console.WriteLine("No se puede calcular el factorial de un numero negativo.");
+        }
+        else
+        {
+            while (i <= numero)
+            {
+                factorial = factorial * i;
+                i = i + 1;
+            }
+
+            Console.WriteLine("El factorial es: " + factorial);
+        }
+    }
+}
