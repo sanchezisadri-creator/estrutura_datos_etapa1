ad263bdc7bf5104bda84d877fa185be568b93ec7
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-06 19:28:23 -0400

    Dice que es el 8 pero es el 9

---
+++ Ejerci9_de_segunda_hoja/Ejerci8_de_segunda_hoja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int contador = 0;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero == 0)
+        {
+            contador = 1;
+        }
+        else
+        {
+            while (numero != 0)
+            {
+                numero = numero / 10;
+                contador = contador + 1;
+            }
+        }
+
+        Console.WriteLine("La cantidad de digitos es: " + contador);
+    }
+}
