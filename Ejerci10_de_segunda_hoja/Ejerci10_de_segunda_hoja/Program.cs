0be2fd45b8030f04ee9c821c7e2476b7e2ac618b
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-06 19:35:28 -0400

    El 10

---
+++ Ejerci10_de_segunda_hoja/Ejerci10_de_segunda_hoja/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+        int digitos;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (numero != 0)
+        {
+            digitos = numero % 10;
+            suma = suma + digitos;
+            numero = numero / 10;
+        }
+
+        Console.WriteLine("La suma de los digitos es: " + suma);
+    }
+}
