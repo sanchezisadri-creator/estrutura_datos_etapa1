403cd8b5b2658ba35f510b270c22a183f2fa93b4
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-06 19:48:03 -0400

    El 12

---
+++ Ejerci12_de_segunda_hoja/Ejerci12_de_segunda_hoja/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+        int a = 0;
+        int b = 1;
+        int c;
+        int contador = 0;
+
+        Console.WriteLine("Ingrese la cantidad de numeros Fibonacci a mostrar: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        while (contador < n)
+        {
+            Console.WriteLine(a);
+
+            c = a + b;
+            a = b;
+            b = c;
+
+            contador = contador + 1;
+        }
+    }
+}
