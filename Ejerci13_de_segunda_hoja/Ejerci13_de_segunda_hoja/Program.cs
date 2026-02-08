91e138dfe6ef1f17bc0efc26fe65ec9a58bbaba6
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-07 20:52:54 -0400

    El 13

---
+++ Ejerci13_de_segunda_hoja/Ejerci13_de_segunda_hoja/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int i = 1;
+        int resultado;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        while (i <= 10)
+        {
+            resultado = numero * i;
+            Console.WriteLine(numero + " x " + i + " = " + resultado);
+            i = i + 1;
+        }
+    }
+}
