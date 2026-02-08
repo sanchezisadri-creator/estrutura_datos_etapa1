12bfe443eaf893107e849ba8789eec3c2a002715
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-07 20:55:35 -0400

    haciendo el 14

---
+++ Ejerci14_de_segunda_hoja/Ejerci14_de_segunda_hoja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int cantidad;
+        int i = 1;
+        int numero;
+        int suma = 0;
+        double promedio;
+
+        Console.WriteLine("¿Cuantos numeros desea ingresar?");
+        cantidad = Convert.ToInt32(Console.ReadLine());
+
+        while (i <= cantidad)
+        {
+            Console.WriteLine("Ingrese el numero " + i + ": ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            suma = suma + numero;
+            i = i + 1;
+        }
+
+        promedio = (double)suma / cantidad;
+
+        Console.WriteLine("El promedio es: " + promedio);
+    }
+}
