90d88149f6012a448379c90617af80ee3827e110
sanchezisadri-creator <sanchezisadri@gmail.com>
2026-02-07 21:02:34 -0400

    15 + 16 Y GRACIAS A DIOS CERRAMOS

---
+++ Ejerci15_de_segunda_hoja/Ejerci15_de_segunda_hoja/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int anio;
+
+        Console.WriteLine("Ingrese un año: ");
+        anio = Convert.ToInt32(Console.ReadLine());
+
+        if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
+        {
+            Console.WriteLine("El año es bisiesto.");
+        }
+        else
+        {
+            Console.WriteLine("El año no es bisiesto.");
+        }
+    }
+}
---
+++ Ejerci16_de_segunda_hoja/Ejerci16_de_segunda_hoja/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string texto;
+        string invertido = "";
+        int i;
+
+        Console.WriteLine("Ingrese una cadena de texto: ");
+        texto = Console.ReadLine();
+
+        i = texto.Length - 1;
+
+        while (i >= 0)
+        {
+            invertido = invertido + texto[i];
+            i = i - 1;
+        }
+
+        Console.WriteLine("Texto invertido: " + invertido);
+    }
+}
